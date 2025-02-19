60522be0e6ceec2bfe68da57a4d9a61126b0a81a
Iwantschenko <160021860+Iwantschenko@users.noreply.github.com>
2025-02-19 17:50:24 +0200

    done task

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -4,9 +4,23 @@
  * @param {string} sourceString
  *
  * @return {object}
- */
-function convertToObject(sourceString) {
-  // write your code here
-}
+  1.Розідялю на classLine -> строки з даними 1 класу
+  2.Видаляю бокові space
+  3.Перевірка на пусті classLine
+  4.Закидаю в об'єкт поле + значення (видаляю для них space якщо залишились)
+*/
 
+function convertToObject(sourceString) {
+  return sourceString
+    .split(';')
+    .map((str) => str.trim())
+    .filter((classLine) => classLine !== '')
+    .reduce((object, classLine) => {
+      const [key, values] = classLine.split(':');
+
+      object[key.trim()] = values.trim();
+
+      return object;
+    }, {});
+}
 module.exports = convertToObject;
